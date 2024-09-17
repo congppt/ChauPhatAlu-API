20f39b2541f3e4d686a5fb3529de05f253308974
congppt <congppt2002@gmail.com>
2024-09-18 03:02:21 +0700

    add shared kernel messaging

---
+++ SharedKernel.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SharedKernel.Messaging.MassTransit;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration config,
+        Assembly? assembly = null)
+    {
+        services.AddMassTransit(cfg =>
+        {
+            cfg.SetKebabCaseEndpointNameFormatter();
+            if (assembly != null) cfg.AddConsumers(assembly);
+            cfg.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(config["MessageBroker:Host"],config["MessageBroker:VirtualHost"], hostConfigurator =>
+                {
+                    hostConfigurator.Username(config["MessageBroker:RabbitMQ:Username"]!);
+                    hostConfigurator.Password(config["MessageBroker:RabbitMQ:Password"]!);
+                });
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+        return services;
+    }
+}
